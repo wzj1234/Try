e777b4fb20b852ebc8a6862a83d8d4d3bd48d1b6
WuZhongjia <18689745139@163.com>
2017-09-26 09:55:35 +0800

    update1_1

--- src/com/action/tryTest.java
+++ src/com/action/tryTest.java
@@ -5,6 +5,7 @@ public class tryTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("tryTest");
+		System.out.println("the fist time to update");
 	}
 
 }
